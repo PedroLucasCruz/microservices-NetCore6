e5240d614e214eb05c8cbdc49a80ad3d8d01a344
PedroLucasCruz <pedrolucas11@live.com>
2022-06-27 22:37:09 -0300

    configurado extension asyncrona para post and put.

---
+++ GeekShooping/GeekShopping.Web/Utils/HttpClienExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+  static  public class HttpClienExtensions
+    {
+      private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+       public static async Task<T> ReadContentAs<T> (
+           this HttpResponseMessage response)
+        {
+            if(!response.IsSuccessStatusCode) throw
+                    new ApplicationException($"Somenthing went sront calling the API: "
+                    + $"{ response.ReasonPhrase}");
+            string dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            JsonSerializerOptions option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            return JsonSerializer.Deserialize<T>(dataAsString, option);
+        }
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsJsonAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsJsonAsync(url, content);
+        }
+    }
+}
+
